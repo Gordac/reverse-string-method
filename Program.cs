22ecccace11006e02042e2b4d080fffdec7c27b0
Gordac <35762008+Gordac@users.noreply.github.com>
2020-01-30 18:01:19 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,19 +6,18 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-			Console.WriteLine("Write something to be reversed...");
-            string input = Console.ReadLine();
-			Console.WriteLine(Reverse(input));
+		Console.WriteLine("Write something to be reversed...");
+            	string input = Console.ReadLine();
+		Console.WriteLine(Reverse(input));
         }
 		
-		static string Reverse (string word){
-			string drow = null;
-			
-			for(int i = word.Length - 1; i >= 0; i--){
-				drow += word[i];
-			}
-			
-			return drow;
+	static string Reverse (string word){
+		string drow = null;
+		for(int i = word.Length - 1; i >= 0; i--){
+		drow += word[i];
 		}
+			
+		return drow;
+	}
     }
 }
