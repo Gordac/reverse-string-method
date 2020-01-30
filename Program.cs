96710ef22ad7015f36718ef138ca87546b553b13
Gordac <35762008+Gordac@users.noreply.github.com>
2020-01-30 17:55:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Write something to be reversed...");
+            string input = Console.ReadLine();
+			Console.WriteLine(Reverse(input));
+        }
+		
+		static string Reverse (string word){
+			string drow = null;
+			
+			for(int i = word.Length - 1; i >= 0; i--){
+				drow += word[i];
+			}
+			
+			return drow;
+		}
+    }
+}
